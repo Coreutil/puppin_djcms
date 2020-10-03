d1df54cf3f9d92e8a4427f5b8a122ce64f59a42c
Polina <staub.zeit@gmail.com>
2020-10-02 18:12:02 -0700

    ADD * added side content information: categories list, recent posts

--- puppin_djcms/apps/blog/templatetags/extra_cms_tags.py
+++ puppin_djcms/apps/blog/templatetags/extra_cms_tags.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from django import template
+from django.db.models import Count
+from django.utils import timezone
+
+from ..models.blog_post import BlogPost
+from ..models.blog_category import BlogCategory
 
 
 register = template.Library()
@@ -11,3 +16,20 @@ def get_user_name(user):
     if user and user.first_name or user.last_name:
         return '%s %s' % (user.first_name, user.last_name)
     return user.username
+
+
+@register.inclusion_tag('blog/categories_list.html', takes_context=True, name='categories_list')
+def get_all_categories(context):
+    categories = BlogCategory.objects.filter(
+        blog_post__published=True, blog_post__publication_datetime__lte=timezone.now()
+    ).annotate(num_posts=Count('blog_post'))
+    context.update({'categories': categories})
+    return context
+
+
+@register.inclusion_tag('blog/recent_posts_list.html', takes_context=True, name='recent_posts')
+def get_recent_posts(context):
+    posts = BlogPost.objects.filter(
+        published=True, publication_datetime__lte=timezone.now())[:4]
+    context.update({'recent_posts': posts})
+    return context
